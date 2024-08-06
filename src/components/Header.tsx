70b629d1573f1910fc6aa75a65051172011d8a4d
Israel Gomes <israelgomes13@gmail.com>
2024-08-05 23:59:34 -0300

    feat: add lucide dependencie for icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,23 @@
+import { Heart } from "lucide-react";
 import Link from "next/link";
 
 export function Header({}) {
   return (
-    <div>
-      <input type="text" placeholder="Buscar usuário" />
+    <header>
+      <div className="flex justify-between pl-6">
+        <input type="text" placeholder="Buscar usuário" />
 
-      <Link href="/favorites">
-        <div className="bg-primary p-6 text-secondary-LIGHT font-medium">
-          Favoritos
-        </div>
-      </Link>
+        <nav>
+          <Link href="/favorites">
+            <div className="flex gap-1 bg-primary p-6 text-secondary-LIGHT font-medium">
+              <Heart />
+              Favoritos
+            </div>
+          </Link>
+        </nav>
+      </div>
 
-      <hr></hr>
-    </div>
+      <hr />
+    </header>
   );
 }
