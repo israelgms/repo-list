ae625321f3bd86eaac01bc7ce34b694acf9ff550
Israel Gomes <israelgomes13@gmail.com>
2024-08-06 23:43:22 -0300

    chore: create get repository list function

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,7 +1,17 @@
+import githubApi from "@/services/githubApi";
+
 export async function getRepositoryListByUserName({
   userName,
 }: {
   userName?: string;
 }) {
   if (!userName) return [];
+
+  try {
+    const response = await githubApi.get(`/users/${userName}/repos`);
+
+    return response.data;
+  } catch (error) {
+    console.log('error', error)
+  }
 }
