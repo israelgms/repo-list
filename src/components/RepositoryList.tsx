d68071766441e7fda01e2aab0991570758c7315a
Israel Gomes <israelgomes13@gmail.com>
2024-08-06 23:44:31 -0300

    feat: create RepositoryList component

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,13 +1,24 @@
 import { getRepositoryListByUserName } from "@/app/actions";
+import { RepositoryItem } from "./RepositoryItem";
+import React from "react";
 
 interface RepositoryListProps {
   userName?: string;
 }
 
 export async function RepositoryList({ userName }: RepositoryListProps) {
-  const services = await getRepositoryListByUserName({
+  const repositoryList = await getRepositoryListByUserName({
     userName,
   });
 
-  return <></>;
+  return (
+    <div className="flex flex-col gap-4">
+      <h1>Repositórios</h1>
+      {repositoryList?.map((repository) => (
+        <React.Fragment key={repository.id}>
+          <RepositoryItem {...repository} />
+        </React.Fragment>
+      ))}
+    </div>
+  );
 }
